35e10944175af883e1ae19355e6269f8f02eac0c
negroiualexandru <negroiualexandru05@gmail.com>
2024-10-12 13:53:11 -0700

    push to make sure i dont mess things up

--- app/src/main/java/com/example/studybuds/MainActivity.java
+++ app/src/main/java/com/example/studybuds/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.studybuds;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //get variables
+    ImageButton homeButton, mapButton, classesButton;
+    ScrollView homePage, mapPage, classesPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //margins of the screen
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //assign all android views to java variables
+        homeButton = findViewById(R.id.homeButton);
+        mapButton = findViewById(R.id.mapButton);
+        classesButton = findViewById(R.id.classesButton);
+        homePage = findViewById(R.id.homePage);
+        mapPage = findViewById(R.id.mapPage);
+        classesPage = findViewById(R.id.classesPage);
+
+        //switch pages on click
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
