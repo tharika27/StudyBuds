b87b5a9bcfae37cc7db5a0ab30c06b2349bea379
negroiualexandru <negroiualexandru05@gmail.com>
2024-10-13 04:54:49 -0700

    switch statement

--- app/src/main/java/com/example/studybuds/StudyLocation.java
+++ app/src/main/java/com/example/studybuds/StudyLocation.java
@@ -18,22 +18,32 @@ public class StudyLocation {
         this.limitOfPeople = limitOfPeople;
         this.locationName = locationName;
 
-        if (this.locationName.equals("Suzzalo")) {
-            this.locationCoordinates = new LatLng(47.6558, -122.3078);
-        } else if (this.locationName.equals("Odegaard")) {
-            this.locationCoordinates = new LatLng(47.6565, -122.3104);
-        } else if (this.locationName.equals("CSE 1")) {
-            this.locationCoordinates = new LatLng(47.6534, -122.3061);
-        } else if (this.locationName.equals("Paccar")) {
-            this.locationCoordinates = new LatLng(47.6594, -122.3090);
-        } else if (this.locationName.equals("CSE 2") || this.locationName.equals("Microsoft Cafe")) {
-            this.locationCoordinates = new LatLng(47.65309, -122.30440);
-        } else if (this.locationName.equals("Loew Hall")) {
-            this.locationCoordinates = new LatLng(47.65440, -122.30443);
-        } else if (this.locationName.equals("HUB")){
-            this.locationCoordinates = new LatLng(47.65540,-122.30507);
-        } else {
-            this.locationCoordinates = new LatLng(47.6567,-122.3066);
+        switch (this.locationName) {
+            case "Suzzalo":
+                this.locationCoordinates = new LatLng(47.6558, -122.3078);
+                break;
+            case "Odegaard":
+                this.locationCoordinates = new LatLng(47.6565, -122.3104);
+                break;
+            case "CSE 1":
+                this.locationCoordinates = new LatLng(47.6534, -122.3061);
+                break;
+            case "Paccar":
+                this.locationCoordinates = new LatLng(47.6594, -122.3090);
+                break;
+            case "CSE 2":
+            case "Microsoft Cafe":
+                this.locationCoordinates = new LatLng(47.65309, -122.30440);
+                break;
+            case "Loew Hall":
+                this.locationCoordinates = new LatLng(47.65440, -122.30443);
+                break;
+            case "HUB":
+                this.locationCoordinates = new LatLng(47.65540, -122.30507);
+                break;
+            default:
+                this.locationCoordinates = new LatLng(47.6567, -122.3066);
+                break;
         }
     }
 }
